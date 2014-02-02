3fb169fadd2e2f9a6df7ab9dc80509c8a255a00b
Ian Loke <ianlokejj@gmail.com>
2014-02-02 22:24:47 +0800

    attempt 2

--- tut_2/q4.cpp
+++ tut_2/q4.cpp
@@ -1,108 +1,54 @@
 #include<iostream>
 #include<string>
+#define MIN_STRING_SIZE 5
+
 using namespace std;
 
-
-// Identify which part of string a is in string b
-// return -1 if not found
-int CompareAndConcat(string a, string b){
+string CompareAndConcat(string a, string b){
 	
 	int pos = -1;
 	string a_substring;
 
-	cout << "COMPARING      " << a << "      AND       " << b << endl;
-
-	for(int i = a.length(); i > 5  && pos == -1 ; i--){
+	for(int i = a.length(); i > MIN_STRING_SIZE  && pos == -1 ; i--){
 		a_substring = a.substr(0, i);
-		cout << "a_substring: " << a_substring << endl;
 		pos = b.find(a_substring);
-		cout << "pos: " << pos << endl;
 	}
 
-	if(pos == -1){
+	if(pos > 0)
+		return b.substr(0, pos) + a;
+
+	else{
 		string b_substring;
 		for(int i = b.length(); i > 5  && pos == -1 ; i--){
 			b_substring = b.substr(0, i);
-			cout << "b_substring: " << b_substring << endl;
 			pos = a.find(b_substring);
-			cout << "pos: " << pos << endl;
 		}
 	}
 
-	return pos;
+	if(pos > 0)
+		return a.substr(0, pos) + b;
+	else
+		return "";
+
 }
 
 string form(string s_1, string s_2, string s_3){
-	string new_string;
-
 	
-	//form substrings
 	string substring_1, substring_2, substring_3;
 	
-	int s_1_position, s_2_position, s_3_position = 0;
+	substring_1 = CompareAndConcat(s_1, s_2);
+	substring_2 = CompareAndConcat(s_1, s_3);
+	substring_3 = CompareAndConcat(s_2, s_3);
 
+	string final;
+	if(substring_1 == "")
+		final = CompareAndConcat(substring_2, substring_3);
+	else if( substring_2 == "")
+		final = CompareAndConcat(substring_1, substring_3);
+	else
+		final = CompareAndConcat(substring_1, substring_2);
 
-	cout << "s_1: " << s_1 << endl;
-	cout << "s_2: " << s_2 << endl;
-	cout << "s_3: " << s_3 << endl;
-
-
-	int position;
-	string s;
-
-	position = CompareAndConcat(s_1, s_2);
-	if (position > 0){
-
-		substring_2 = s_2.substr(0, position) + s_1;
-		cout << "substring_2: " << substring_2 << endl;
-	}
-	
-	position = CompareAndConcat(s_1, s_3);
-	if (position > 0){
-		substring_3 = s_3.substr(0, position) + s_1;
-		cout << "substring_3: " << substring_3 << endl;
-	}
-
-//	position = CompareAndConcat(s_2, s_1);
-//	if (position > 0){
-//		substring_1 = s_1.substr(0, position) + s_2;
-//		cout << "substring_1: " << substring_1 << endl;
-//	}
-
-	position = CompareAndConcat(s_2, s_3);
-	if (position > 0){
-		substring_3 = s_3.substr(0, position) + s_2;
-		cout << "substring_3: " << substring_3 << endl;
-	}
-
-//	position = CompareAndConcat(s_3, s_1);
-//	if (position > 0){
-//		substring_1 = s_1.substr(0, position) + s_3;
-//		cout << "substring_1: " << substring_1 << endl;
-//	}
-
-//	position = CompareAndConcat(s_3, s_2);	
-//	if (position > 0){
-//		substring_2 = s_2.substr(0, position) + s_3;		
-//		cout << "substring_2: " << substring_2 << endl;
-//	}
-
-
-	cout << "substring_1: " << substring_1 << endl;
-	cout << "substring_2: " << substring_2 << endl;
-	cout << "substring_3: " << substring_3 << endl;
-	
-	if(substring_1 == ""){
-		CompareAndConcat(substring_2, substring_3);
-	}
-
-
-	int final = CompareAndConcat(substring_2, substring_3);
-	cout << "final: " << s << endl;
-	// compare string 1 and string 2
-
-
-	return s;
+	return final;
 }
 
 
@@ -110,13 +56,11 @@ int main(){
 	
 	string s_1, s_2, s_3;
 
-	s_3 = "fox jumps over the lazy dog, while the five hexing wi";
-	s_1 = ", while the five hexing wizard bots jump quickly";
-	s_2 = "the quick brown fox jumps over the lazy d";
+	s_1 = "fox jumps over the lazy dog, while the five hexing wi";
+	s_2 = ", while the five hexing wizard bots jump quickly";
+	s_3 = "the quick brown fox jumps over the lazy d";
 
-	form(s_1, s_2, s_3);
-
+	cout << form(s_1, s_2, s_3) << endl;
 
 	return 0;
-
 }
